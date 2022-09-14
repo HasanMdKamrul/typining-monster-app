b1d77124029141bbba692854566c8d5cbcbdbb32
HasanMdKamrul <kamrulhasan@iut-dhaka.edu>
2022-09-14 20:52:14 +0200

    Responsiveness for all 3 devices added and configured

--- script.js
+++ script.js
@@ -138,5 +138,5 @@ setInterval(() => {
 
   
 
-  document.getElementById("show-time").innerHTML = `${startTime ? parseInt(timeSpent) + '.00' : 0} seconds`;
+  document.getElementById("show-time").innerHTML = `${startTime ? parseInt(timeSpent) : 0} seconds`;
 }, 1000);
